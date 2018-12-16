3e4a2ea24e35e40c96a6c1c7c27b601dbf1a5a5e
jstahovic <jstahovicjr@gmail.com>
2018-12-16 17:56:35 -0500

    Beginnings of selectively pulling in the author's Lemon Mart pieces.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { MaterialModule } from './material.module'
 import { ManagerModule } from './manager/manager.module'
 import { InventoryModule } from './inventory/inventory.module'
 import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
